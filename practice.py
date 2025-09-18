54cf2c13d8889e6bf54dc7c9421bc431f2e316e2
unknown <saechaek59@gmail.com>
2025-09-18 16:37:00 +0900

    try2

--- practice.py
+++ practice.py
@@ -1,11 +1,13 @@
 from typing import List
 import math
+from abc import ABC, abstractmethod
 
-class Shape:
+class Shape(ABC):
     # TODO: Define the method "measure".
     """
     - This is an abstract method, so it should just raise NotImplementedError.
     """
+    @abstractmethod
     def measure():
         pass
 
@@ -18,29 +20,29 @@ class Circle(Shape):
         self.radius = radius
 
     def measure(self):
-        return pi * self.radius ** 2
+        return math.pi * self.radius ** 2
 
 
 # TODO: Create a Rectangle class that inherits from Shape.
 # - Initialize it with width and height (floats).
 # - Implement the measure method to return the area of the rectangle.
 class Rectangle(Shape):
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     def measure(self):
-        return self.w * self.h
+        return self.width * self.height
 
 
 # TODO: Create a Triangle class that inherits from Shape.
 # - Initialize it with three vertices, each a tuple of (x, y).
 # - Implement the measure method to return the area of the triangle using Heron's formula.
 class Triangle(Shape):
-    def __init__(self, a, b, c):
-        self.a = (x1, y1)
-        self.b = (x2, y2)
-        self.c = (x3, y3)
+    def __init__(self, p1, p2, p3):
+        self.a = p1
+        self.b = p2
+        self.c = p3
 
     def measure(self):
         A = math.sqrt((self.a[0] - self.b[0]) ** 2 + (self.a[1] - self.b[1]) ** 2)
