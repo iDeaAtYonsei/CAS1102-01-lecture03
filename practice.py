d145989ef3aa86bad365dfa12085d5923424af40
JustALeek <alexseo0525@gmail.com>
2025-09-18 16:14:31 +0900

    commit1

--- practice.py
+++ practice.py
@@ -1,28 +1,45 @@
+from abc import *
+import math
 from typing import List
 
 
-class Shape:
-    # TODO: Define the method "measure".
-    """
-    - This is an abstract method, so it should just raise NotImplementedError.
-    """
+class Shape(ABC):
+    @abstractmethod
+    def measure():
+        raise NotImplementedError("Subclasses must implement this method.")
 
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+    
+    def measure(self):
+        return math.pi * self.__radius * self.__radius
 
-# TODO: Create a Circle class that inherits from Shape.
-# - Initialize it with a radius (float).
-# - Implement the measure method to return the area of the circle.
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__length = height
+    
+    def measure(self):
+        return self.__width*self.__length
+    
+class Triangle():
+    def __init__(self, p1, p2, p3):
+        self.__p1 = p1
+        self.__p2 = p2
+        self.__p3 = p3
 
-
-# TODO: Create a Rectangle class that inherits from Shape.
-# - Initialize it with width and height (floats).
-# - Implement the measure method to return the area of the rectangle.
-
-
-# TODO: Create a Triangle class that inherits from Shape.
-# - Initialize it with three vertices, each a tuple of (x, y).
-# - Implement the measure method to return the area of the triangle using Heron's formula.
-
-
+    def findLength(self, v1, v2):
+        return math.sqrt((v2[0] - v1[0])**2 + (v2[1] - v1[1])**2)
+        
+    def measure(self):
+        a = self.findLength(self.__p1, self.__p2)
+        b = self.findLength(self.__p1, self.__p3)
+        c = self.findLength(self.__p2, self.__p3)
+        s = (a+b+c)/2
+        print(math.sqrt(s * (s-a) * (s-b) * (s-c)))
+        return math.sqrt(s*(s-a)*(s-b)*(s-c))
+    
 if __name__ == "__main__":
     shapes: List[Shape] = [
         Circle(radius=1.0),
