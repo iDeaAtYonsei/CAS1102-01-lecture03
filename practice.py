ac901cb13d35c2b3ff5b89f6c98bcd2832722fb7
Minjae Park <bagmi@DESKTOP-9JDGCBS.localdomain>
2025-09-18 15:49:39 +0900

    2025150066

--- practice.py
+++ practice.py
@@ -6,22 +6,44 @@ class Shape:
     """
     - This is an abstract method, so it should just raise NotImplementedError.
     """
-
+    def measure():
+        raise NotImplementedError
 
 # TODO: Create a Circle class that inherits from Shape.
 # - Initialize it with a radius (float).
 # - Implement the measure method to return the area of the circle.
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def measure(self):
+        return 3.14 * (self.radius ** 2)
 
 
 # TODO: Create a Rectangle class that inherits from Shape.
 # - Initialize it with width and height (floats).
 # - Implement the measure method to return the area of the rectangle.
-
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def measure(self):
+        return self.width * self.height
 
 # TODO: Create a Triangle class that inherits from Shape.
 # - Initialize it with three vertices, each a tuple of (x, y).
 # - Implement the measure method to return the area of the triangle using Heron's formula.
-
+class Triangle(Shape):
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+    def measure(self):
+        a = (((self.p1[0]-self.p2[0])**2) + ((self.p1[1]-self.p2[1])**2))**0.5
+        b = (((self.p1[0]-self.p3[0])**2) + ((self.p1[1]-self.p3[1])**2))**0.5
+        c = (((self.p3[0]-self.p2[0])**2) + ((self.p3[1]-self.p2[1])**2))**0.5
+        s = (a+b+c)/2
+        area = (s*(s-a)*(s-b)*(s-c))**0.5
+        return area
 
 if __name__ == "__main__":
     shapes: List[Shape] = [
